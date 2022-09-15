0e80d397c0928f8869d67e43959471088631ddd5
PC-10121024\hoon <imode7@naver.com>
2022-09-15 10:52:23 +0900

    6.3 완료

---
+++ 5-generic/5-1-function.ts
@@ -0,0 +1,26 @@
+{
+  function checkNotNullBad(arg: number | null): number {
+    if (arg == null) {
+      throw new Error("not valid number!");
+    }
+    return arg;
+  }
+
+  function checkNotNullAnyBad(arg: any | null): any {
+    if (arg == null) {
+      throw new Error("not valid number!");
+    }
+    return arg;
+  }
+  const result = checkNotNullAnyBad(123);
+
+  // generic => 통상적인, 일반적인, 다 포용하는
+  function checkNotNull<T>(arg: T | null): T {
+    if (arg == null) {
+      throw new Error("not valid number!");
+    }
+    return arg;
+  }
+  const number = checkNotNull(123);
+  const boal: boolean = checkNotNull(true);
+}

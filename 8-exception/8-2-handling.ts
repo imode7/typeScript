34e3860bccb3f48fba3da535713f61320d6ad3c4
PC-10121024\hoon <imode7@naver.com>
2022-09-20 09:39:08 +0900

    9.4 완료

--- 8-exception/8-2-handling.ts
+++ 8-exception/8-2-handling.ts
@@ -14,7 +14,17 @@ class UserService {
 }
 
 class App {
-  constructor(props) {}
-
-  run();
+  constructor(private userService: UserService) {}
+  run() {
+    try {
+      this.userService.login();
+    } catch (error) {
+      // show dialog to user
+    }
+  }
 }
+
+const client = new NetworkClient();
+const service = new UserService(client);
+const app = new App(service);
+app.run();

37e7b4cd3f60e4c2a8917e60b544c8b3a5f8f671
PC-10121024\hoon <imode7@naver.com>
2022-09-26 11:00:51 +0900

    14.10 컴포넌트 리팩토링💡 (베이스 컴포넌트 만들기)

--- motion/src/components/page/item/image.ts
+++ motion/src/components/page/item/image.ts
@@ -1,11 +1,10 @@
-export class ImageComponent {
-  private element: HTMLElement;
+import { BaseComponent } from "../../component.js";
+
+export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
-    const template = document.createElement("template");
-    template.innerHTML = `<section class="image">
+    super(`<section class="image">
     <div class="image__holder"><img class="image__thumbnail"></div>
-    <p class="image__title"></p></section>`;
-    this.element = template.content.firstElementChild! as HTMLElement;
+    <p class="image__title"></p></section>`);
 
     const imageElement = this.element.querySelector(
       ".image__thumbnail"
@@ -19,8 +18,5 @@ export class ImageComponent {
     ) as HTMLTitleElement;
 
     titleElement.textContent = title;
-  }
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    parent.insertAdjacentElement(position, this.element);
   }
 }

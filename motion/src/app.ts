0f5a168369ce4f3a4ef95dc02dc73de166376657
PC-10121024\hoon <imode7@naver.com>
2022-09-27 09:28:15 +0900

    14.14 모든 섹션들 구현하기💡 (유튜브 URL 만들기편)

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,5 +1,8 @@
 import { PageComponent } from "./components/page/page.js";
 import { ImageComponent } from "./components/page/item/image.js";
+import { NoteComponent } from "./components/page/item/note.js";
+import { TodoComponent } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
 
 class App {
   private readonly page: PageComponent;
@@ -12,6 +15,18 @@ class App {
       "https://picsum.photos/600/300"
     );
     image.attachTo(appRoot, "beforeend");
+
+    const video = new VideoComponent(
+      "Video Title",
+      "https://youtu.be/K3-jG52XwuQ"
+    );
+    video.attachTo(appRoot, "beforeend");
+
+    const note = new NoteComponent("Note Title", "Note Body");
+    note.attachTo(appRoot, "beforeend");
+
+    const todo = new TodoComponent("Todo Title", "Todo Item");
+    todo.attachTo(appRoot, "beforeend");
   }
 }
 
---
+++ motion/src/components/page/item/video.ts
@@ -0,0 +1,41 @@
+import { BaseComponent } from "../../component.js";
+
+export class VideoComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="video">
+                        <div class="video__player"><iframe class="video__iframe"></iframe></div>
+                        <h3 class="video__title"></h3>
+                    </section>`);
+
+    const iframe = this.element.querySelector(
+      ".video__iframe"
+    )! as HTMLIFrameElement;
+
+    iframe.src = this.convertToEmbeddedURL(url);
+
+    const titleElement = this.element.querySelector(
+      ".video__title"
+    )! as HTMLHeadingElement;
+    titleElement.textContent = title;
+  }
+
+  // 정규표현식 Regex
+  private convertToEmbeddedURL(url: string): string {
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
+  }
+}
+
+/*
+<iframe width="2114" height="872"
+src="https://www.youtube.com/embed/dFCegRdFo6U"
+title="지진 처음 겪고 지진 드립치는데 또 지진이 오냐"
+frameborder="0"
+allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+</iframe>*/

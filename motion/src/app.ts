6a71e7991f7b01a43924e23d0b63dbbd468dddc8
PC-10121024\hoon <imode7@naver.com>
2022-09-28 09:15:28 +0900

    14.23 드디어 텅빈 다이얼로그 만들기 💡

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -8,6 +8,7 @@ import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
 import { Component } from "./components/component.js";
+import { InputDialog } from "./components/dialog/dialog.js";
 
 class App {
   private readonly page: Component & Composable;
@@ -32,6 +33,21 @@ class App {
 
     const todo = new TodoComponent("Todo Title", "Todo  Item");
     this.page.addChild(todo);
+
+    const imageBtn = document.querySelector("#new-image")! as HTMLButtonElement;
+    imageBtn.addEventListener("click", () => {
+      const dialog = new InputDialog();
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      });
+      dialog.setOnSubmitListener(() => {
+        // 섹션을 만들어서 페이지에 추가 해준다.
+        dialog.removeFrom(document.body);
+      });
+
+      dialog.attachTo(document.body);
+    });
   }
 }
 
